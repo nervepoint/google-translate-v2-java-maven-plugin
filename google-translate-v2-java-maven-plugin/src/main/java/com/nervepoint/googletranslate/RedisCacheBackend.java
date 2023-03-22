8f871b1cf6437d1a0efb60c00abf319851a525ac
Brett Smith <brett@nervepoint.com>
2023-03-22 15:13:12 +0000

    Stray import.

--- google-translate-v2-java-maven-plugin/src/main/java/com/nervepoint/googletranslate/RedisCacheBackend.java
+++ google-translate-v2-java-maven-plugin/src/main/java/com/nervepoint/googletranslate/RedisCacheBackend.java
@@ -2,7 +2,6 @@ package com.nervepoint.googletranslate;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
